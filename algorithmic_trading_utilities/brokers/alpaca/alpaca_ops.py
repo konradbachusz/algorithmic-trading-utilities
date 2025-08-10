5e9f4d711b1d82be67e886f0cbf6f1dcead8e415
konradbachusz <konradbachusz@gmail.com>
2025-08-10 08:47:41 +0100

    Refactor: Remove unused modules and implement Alpaca data operations

---
+++ algorithmic_trading_utilities/brokers/alpaca/alpaca_ops.py
@@ -0,0 +1,8 @@
+#TODO fix
+# import sys
+# sys.path.insert(1, "algorithmic_trading_utilities")
+
+# from common.viz_ops import plot_time_series
+# from data.data_yahoo_finance import get_sp500_prices
+
+# print(get_sp500_prices("2023-01-01"))
