ff91582c7908aaaac09efd4573bcb4328aa4c496
konradbachusz <konradbachusz@gmail.com>
2025-09-13 17:06:59 +0100

    Retreiving an article

--- algorithmic_trading_utilities/common/news_ops.py
+++ algorithmic_trading_utilities/common/news_ops.py
@@ -1,13 +1,30 @@
 from crewai_tools import ScrapeWebsiteTool
 
-# To enable scrapping any website it finds during it's execution
-tool = ScrapeWebsiteTool()
 
-# Initialize the tool with the website URL, 
-# so the agent can only scrap the content of the specified website
-tool = ScrapeWebsiteTool(website_url='https://finance.yahoo.com/news')
+def scrape_page(url):
+    """
+    Scrapes the content of a specified webpage.
 
-# Extract the text from the site
-text = tool.run()
-print(text)
+    This function utilizes the ScrapeWebsiteTool to extract text from the
+    webpage at the given URL. It initializes the tool with the URL and
+    executes the scraping process, returning the extracted text.
 
+    Args:
+        url (str): The URL of the webpage to scrape.
+
+    Returns:
+        str: The text content extracted from the webpage.
+    """
+
+    # To enable scrapping any website it finds during it's execution
+    tool = ScrapeWebsiteTool()
+
+    # Initialize the tool with the website URL,
+    # so the agent can only scrap the content of the specified website
+    tool = ScrapeWebsiteTool(website_url=url)
+
+    # Extract the text from the site
+    scraped_page_text = tool.run()
+    return scraped_page_text
+
+print(scrape_page("https://en.wikipedia.org/wiki/Brain%E2%80%93computer_interface"))
