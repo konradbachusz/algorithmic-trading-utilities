738cfa3b3a26703e36f6870ed206b68b97d7c3f8
Yosri Ben Halima <yosri@laevitas.ch>
2025-09-22 09:15:51 +0100

    Integrate sample_data pytest fixture in conftest

--- tests/test_viz_ops.py
+++ tests/test_viz_ops.py
@@ -1,28 +1,8 @@
-import pytest
-import numpy as np
 import pandas as pd
-from algorithmic_trading_utilities.common.portfolio_ops import PerformanceMetrics
 from algorithmic_trading_utilities.common.viz_ops import PerformanceViz
 
 
 class TestPerformanceViz:
-    @pytest.fixture
-    def sample_data(self):
-        np.random.seed(42)
-        dates = pd.date_range(start="2025-01-01", periods=252)
-        portfolio_returns = np.random.normal(0.0005, 0.01, len(dates))
-        portfolio_equity = pd.Series(
-            10000 * np.cumprod(1 + portfolio_returns), index=dates
-        )
-
-        benchmark_returns = np.random.normal(0.0003, 0.008, len(dates))
-        benchmark_equity = pd.Series(
-            10000 * np.cumprod(1 + benchmark_returns), index=dates
-        )
-
-        pm = PerformanceMetrics(portfolio_equity, benchmark_equity)
-        return pm, portfolio_equity, benchmark_equity
-
     def test_init_with_pm(self, sample_data):
         pm, portfolio, benchmark = sample_data
         viz = PerformanceViz(pm=pm)
@@ -32,7 +12,7 @@ class TestPerformanceViz:
         assert viz.benchmark.index.equals(benchmark.index)
 
     def test_plot_equity_curve(self, sample_data):
-        pm, _, benchmark = sample_data
+        pm, _, _ = sample_data
         viz = PerformanceViz(pm=pm)
         fig = viz.plot_equity_curve(show=False)
         assert fig is not None
